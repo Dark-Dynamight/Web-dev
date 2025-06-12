a8a88a201f3f2af670db09cc225895fe9c14a238
Dark-Dynamight <payyaulasurya039@gmail.com>
2025-06-12 14:01:38 +0530

    Javascript variables & datatypes

---
+++ javascript/class1/1.js
@@ -0,0 +1,44 @@
+console.log("hello world")
+
+let a
+
+console.log("the value of a is ",a)
+console.log("the Datatype of a is ",typeof a)
+// number
+a=10
+console.log("the value of a is ",a)
+console.log("the Datatype of a is ",typeof a)
+// boolean
+a=true
+console.log("the value of a is ",a)
+console.log("the Datatype of a is ",typeof a)
+// string
+a="hi"
+console.log("the value of a is ",a)
+console.log("the Datatype of a is ",typeof a)
+//array
+let arr=[10,20,30]
+console.log("the value of arr is ",arr)
+console.log("the Datatype of arr is ",typeof a)
+//object
+let person={
+    name:'surya',
+    id:495
+}
+console.log("the value of person is ",person)
+console.log("the Datatype of person is ",typeof person)
+//function
+function sumof(){
+    return 5;
+}
+
+console.log("the value of sumof is ",sumof)
+console.log("the Datatype of sumof is ",typeof sumof)
+
+
+//operatores
+//arthematic exponentation
+let x=10
+let y=2
+let result=x**y
+console.log(result)
