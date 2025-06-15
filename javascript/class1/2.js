be989c8fa0c86ffb726880658c7c982a0ea0b831
Dark-Dynamight <payyaulasurya039@gmail.com>
2025-06-15 20:28:09 +0530

    day11

---
+++ javascript/class1/2.js
@@ -0,0 +1,36 @@
+//descison maker
+if(2%2==0){
+    console.log("hi")
+}
+else if(3>=3){
+    console.log("hello")
+}
+else{
+    console.log("goodbye")
+}
+
+//loops
+for(let i=0;i<5;i++){
+    console.log("world")
+}
+let i=0
+while(i<5){
+    console.log("boom")
+    i++
+}
+//switch case
+let choice=3
+switch(choice){
+    case 1:console.log("1")
+    break
+    case 2:console.log("2")
+    break
+    case 3:console.log("3")
+    break
+}
+//function
+function multiplicationOfTwoNumbers (a,b){
+    return a*b
+}
+let c=multiplicationOfTwoNumbers(5,6)
+console.log(c)
