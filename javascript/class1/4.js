4f9fe113690bcf9f9c0de9f8a9610de058f2afc0
Dark-Dynamight <payyaulasurya039@gmail.com>
2025-06-17 23:09:32 +0530

    day12

---
+++ javascript/class1/4.js
@@ -0,0 +1,9 @@
+//normal function
+let productOfTwo=function (a,b){
+    return a*b;
+}
+// arrow function
+let productOfTwo1=(a,b)=>a*b
+
+console.log(productOfTwo(2,3))
+console.log(productOfTwo1(3,3))
