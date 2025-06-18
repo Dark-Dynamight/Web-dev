dabe55a26a544b6f5f0ad415baeafc70652fc91e
Dark-Dynamight <payyaulasurya039@gmail.com>
2025-06-18 21:57:38 +0530

    day13

--- javascript/class1/4.js
+++ javascript/class1/4.js
@@ -7,3 +7,43 @@ let productOfTwo1=(a,b)=>a*b
 
 console.log(productOfTwo(2,3))
 console.log(productOfTwo1(3,3))
+
+//arrays
+let names=['surya','karthik','sai']
+let arr=[1,2,3,4]
+let arr1=[1,'hi',true]
+
+console.log(names)
+console.log(arr)
+console.log(arr1)
+
+//print whole array
+
+for(let i=0;i<arr.length;i++){
+    // console.log(arr[i])
+}
+
+//for-of
+for(let v of arr){
+    console.log(v)
+}
+
+// Array operations
+// insert at start
+arr.unshift(3)
+console.log(arr)
+// insert at end
+arr.push(5)
+console.log(arr)
+//insert in between
+arr.splice(3,0,5,10)
+console.log(arr)
+//delete at start
+arr.shift()
+console.log(arr)
+//delete at end
+arr.pop()
+console.log(arr)
+//delete in between
+arr.splice(2,1)
+console.log(arr)
