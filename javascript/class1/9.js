c94c17d7eade5b3283e9ba20387d09cbaa843223
Dark-Dynamight <payyaulasurya039@gmail.com>
2025-07-07 21:30:40 +0530

    Arraymethods

---
+++ javascript/class1/9.js
@@ -0,0 +1,18 @@
+let str = "Hello Everyone"
+let str1 = "hello"
+let str20 = "@#$%^B"
+console.log(str1.length)
+console.log(str.replace(" ", "_"))
+console.log(str.slice(1, 5))
+console.log(str.substring(1, 2))
+console.log(str.substr(1, 3))
+console.log(str.replaceAll("l", "u"))
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str.concat(str20))
+console.log(str.charAt(6))
+console.log(str.indexOf("E"))
+console.log(str.split(''))
+console.log(str.split(' '))
+console.log(str.split('  '))
+
