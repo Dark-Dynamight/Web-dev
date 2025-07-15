2b1036a5ec8e2fcf7149acc5bfde80e28985789c
Dark-Dynamight <payyaulasurya039@gmail.com>
2025-07-15 09:52:31 +0530

    Asynchronous

---
+++ javascript/Asynchronous.js
@@ -0,0 +1,9 @@
+// Prints After 5sceonds (5000)
+setTimeout(() => {
+    console.log("5 seconds delay")
+}, 5000);
+
+// Repeats for every 2seconds(2000)
+// setInterval(() => {
+//     console.log("repeat")
+// }, 2000)
